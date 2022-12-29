df816aada942d7a7a108afcaeea895afb9529dcc
YedagneAnicet <yedagneanicet@gmail.com>
2022-12-29 10:55:45 +0000

    exercice_xp revue

--- script.js
+++ script.js
@@ -64,14 +64,16 @@ If there are more than 2 users, console.log the first two names in the users arr
 
 const users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
 
-
-if(users.length === 0){
-    console.log("no one is online");
-
-}else if(users.length === 1){
-    console.log(`${users[0]} is online`);
-}else if(users.length ===2){
-    console.log(`${users[0]} and ${users[1]} are online`);
-}else if(users.length > 2){
+switch (users.length){
+    case 0 :
+        console.log("no one is online");
+        break;
+    case 1 :
+        console.log(`${users[0]} is online`);
+        break;
+    case 2 :
+        console.log(`${users[0]} and ${users[1]} are online`);
+        break;
+    default :
     console.log(`${users[0]}, ${users[1]} and ${(users.length - 2)} users are online`);
 }
